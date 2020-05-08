32518b534adf826acaf86d5e757621e08b9e9182
Can Ni <cni@wario.ece.utexas.edu>
2020-05-08 16:32:53 -0500

    May 8th 3:15pm commit: finished app_agent_in app_agent_out app_only_env and app_only_test definitions in modules.sv; however, in the definition of app_only_env, no subscribers nor scoreborads have been instantiated

--- verif/tb/modules.sv
+++ verif/tb/modules.sv
@@ -2,7 +2,6 @@
 `include "constants.sv"
 package modules_pkg;
 
-
 import uvm_pkg::*;
 import sequences::*;
 
@@ -137,7 +136,107 @@ class app_monitor_out extends uvm_monitor;
 	endtask:run_phase
 endclass:app_moniter_out
 
+class app_agent_in extends uvm_agent;
+// encapsulates a Sequencer, Driver, Monitor (only Monitor if passive) into a single entity by
+// instantiating and connecting the components together via TLM interfaces
+	`uvm_component_utils(app_agent_in);
 
+	uvm_analysis_port #(app_transaction_in) aport;	
+	app_sequencer_in 	app_sequencer_in_0;
+	app_driver_in		app_driver_in_0;		
+	app_monitor_in		app_monitor_in_0;
+	
+	function new(string name, uvm_component parent);
+		super.new(name, parent);
+	endfunction:new
 
+	function void build_phase(uvm_phase phase);
+		aport = new("aport", this);
+		app_sequencer_in_0 	= app_sequencer_in::type_id::create("app_sequencer_in_0", this);
+		app_driver_in_0		= app_driver_in::type_id::create("app_driver_in_0", this);
+		app_monitor_in_0		= app_monitor_in::type_id::create("app_monitor_in_0". this);
+	endfunction:build_phase
 
+	task connect_phase(uvm_phase phase);
+		app_driver_in_0.seq_item_port.connect(app_sequencer_in_0.seq_item_export);
+		app_monitor_in_0.aport.connect(aport);
+	endtask:connect_phase
+endclass: app_agent_in
+
+class app_agent_out extends uvm_agent;
+	`uvm_component_utils(app_agent_out);
+
+	uvm_analysis_port #(app_transaction_out) aport;	
+	app_monitor_out		app_monitor_out_0;
+	
+	function new(string name, uvm_component parent)
+		super.new(name, parent);
+	endfunction:new
+	
+	function void build_phase(uvm_phase phase)
+		aport = new("aport",this);
+		app_monitor_out_0 = app_monitor_out::type_id::create("app_monitor_out_0", this);
+	endfunction:build_phase
+
+	function void connect_phase(uvm_phase phase)
+		app_monitor_out_0.aport.connect(aport);
+	endfunction:connect_phase
+endclass:app_agent_out
+
+class app_only_env extends uvm_env;
+	`uvm_component_utils(top_env);
+	app_agent_in		app_agent_in_0;
+	app_agent_out		app_agent_out_0;
+	// fill in declaration of subscribers
+	// fill in declaration of an Application Layer scoreboard
+	
+	function new(string name, uvm_component parent);
+		super.new(name, parent);
+	endfunction:new
+
+	function void build_phase(uvm_phase phase);
+		app_agent_in_0 = app_agent_in::type_id::create("app_agent_in_0", this);
+		app_agent_out_0 = app_agent_in::type_id::create("app_agent_out_0", this);
+		// fill in instantiation of subscribers
+		// fill in instantiation of an Application Layer scoreboard
+	endfunction: build_phase
+
+	function void connect_phase(uvm_phase phase);
+		// fill in analysis port connection between app_agent_in_0 and subscriber_in	
+		// fill in analysis port connection between app_agent_outt0 and subscriber_out	
+		// fill in analysis port connection between app_agent_in_0 and scoreboard_in	
+		// fill in analysis port connection between app_agent_outt0 and scoreboard_out	
+	endfunction:connect_phase
+
+	function void start_of_simulation_phase(uvm_phase phase);
+		uvm_top.set_report_verbosity_level_hier(UVM_HIGH);
+	endfunction: start_of_simulation_phase
+endclass:app_only_env
+
+class app_only_test extends uvm_test;
+// use the same environment with a different configuration for each test
+// each testcase can override, tweak knobs, enable/disable agents, 
+// change variable values in the configuration table, 
+// and change default sequences for each sequencer in the verification environment
+	`uvm_component_utils(app_only_test);
+	
+	sdr_dut_config 	dut_config_0;
+	app_only_env	top_env_0;
+
+	function new(string name, uvm_component parent);
+		super.new(name, parent);
+	endfunction:new
+
+	function void build_phase(uvm_phase phase);
+		dut_config_0 = new();
+        
+		if(!uvm_config_db #(virtual dut_in)::get( this, "", "dut_vi_in", dut_config_0.dut_vi_in))
+        	  `uvm_fatal("NOVIF", "No virtual interface set for dut_in")
+        	
+        	if(!uvm_config_db #(virtual dut_out)::get( this, "", "dut_vi_out", dut_config_0.dut_vi_out))
+        	  `uvm_fatal("NOVIF", "No virtual interface set for dut_out")
+       
+       		uvm_config_db #(sdr_dut_config)::set(this, "*", "dut_config", dut_config_0);
+	endclass:app_only_test
+endclass:app_only_test
 endpackage:modules_pkg 
